73514139f18c4d755a87ad6586372cb41cce6ff3
Hüseyin ÖLMEZ <detmach@hotmail.com>
2019-02-14 10:12:01 +0300

    Proje dosyası ekle.

---
+++ WinForm/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static WinForm.Program;
+
+namespace WinForm
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private async void btn1_Click(object sender, EventArgs e)
+        {
+            Thread tLonger = new Thread(() => CountLonger(50));
+            tLonger.Start();
+
+            Thread tShorter = new Thread(() => CountShorter(10, 20));
+            tShorter.Start();
+        }
+
+        public void CountLonger(int val)
+        {
+            for (int i = 0; i < val; i++)
+            {
+                Thread.Sleep(50);
+            }
+            MessageBox.Show(val + "'e kadar saydım");
+        }
+
+        public void CountShorter(int val, int wait)
+        {
+            for (int i = 0; i < val; i++)
+            {
+                Thread.Sleep(wait);
+            }
+            MessageBox.Show(val + "'a kadar saydım");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //
+        }
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == NativeMethods.BUNUYAP)
+            {
+                MessageBox.Show("Bunu Yap");
+            }
+            else if (m.Msg == NativeMethods.YADABUNU)
+            {
+                MessageBox.Show("Yada Bunu Yap");
+            }
+            else if (m.Msg == NativeMethods.BUTONABAS)
+            {
+                btn1.PerformClick();
+            }
+            base.WndProc(ref m);
+        }
+    }
+}
---
+++ WinForm/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm
+{
+    static class Program
+    {
+        /// <summary>
+        /// Uygulamanın ana girdi noktası.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            bool kontrol;
+            Mutex mutex = new Mutex(true, "WinForm", out kontrol); //Örnek Mutex nesnesi oluşturalım. 
+            if (kontrol == false)
+            {
+                if (args.Length == 0)
+                {
+                    return;
+                }
+                if (args[0] == "bunuyap")
+                {
+                    NativeMethods.PostMessage(
+               (IntPtr)NativeMethods.HWND_BROADCAST,
+               NativeMethods.BUNUYAP,
+               IntPtr.Zero,
+               IntPtr.Zero);
+                    return;
+                }
+                else if (args[0] == "sunuyap")
+                {
+                    NativeMethods.PostMessage(
+               (IntPtr)NativeMethods.HWND_BROADCAST,
+               NativeMethods.YADABUNU,
+               IntPtr.Zero,
+               IntPtr.Zero);
+                    return;
+                }
+                else if (args[0] == "butonabas")
+                {
+                    NativeMethods.PostMessage(
+               (IntPtr)NativeMethods.HWND_BROADCAST,
+               NativeMethods.BUTONABAS,
+               IntPtr.Zero,
+               IntPtr.Zero);
+                    return;
+                }
+                return;
+            }
+               
+           
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            GC.KeepAlive(mutex);
+
+        }
+        internal class NativeMethods
+        {
+            public const int HWND_BROADCAST = 0xffff;
+            
+            public static readonly int BUNUYAP = RegisterWindowMessage("BUNUYAP");
+            public static readonly int YADABUNU = RegisterWindowMessage("YADABUNU");
+            public static readonly int BUTONABAS = RegisterWindowMessage("BUTONABAS");
+            [DllImport("user32")]
+            public static extern bool PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
+            [DllImport("user32")]
+            public static extern int RegisterWindowMessage(string message);
+        }
+    }
+}
